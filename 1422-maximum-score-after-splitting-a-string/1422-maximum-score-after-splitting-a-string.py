7441f2f4b5af8be02e8009a6ab1fbb4169ca2da4
devanggg05 <110445939+devanggg05@users.noreply.github.com>
2023-12-22 23:17:52 +0530

    Time: 51 ms (20.13%), Space: 17.3 MB (5.69%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
@@ -1,7 +1,8 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        ans = t = (s[0] == '0') + s[1:].count('1')
-        for i in range(1, len(s) - 1):
-            t += 1 if s[i] == '0' else -1
-            ans = max(ans, t)
-        return ans
+        
+        max_score = 0
+        for i in range(1,len(s)):
+            score = s[:i].count('0') + s[i:].count('1')
+            max_score = max(max_score,score)
+        return max_score
